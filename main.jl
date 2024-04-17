e3f138aaea2e6587cb721c4d8af3b56b3773050b
tysondanby <tyson.danby@hotmail.com>
2024-04-16 21:54:49 -0600

    New plot

--- main.jl
+++ main.jl
@@ -7,23 +7,25 @@ function cosslopefunction(x)
     return 1500.0 - 500*cos(pi*x/10000)
 end
 function logslopefunction(x)
-    N = 1000.0
+    N = 1920.24-1249.68
     P = 0.75
     k = 0.0000015
     temp1 = (N-P)/P
     temp2 = exp(-N*x*k)
-    return N/(temp1*temp2 + 1)
+    return N/(temp1*temp2 + 1) + 1249.68
 end
 
 m0 = 1000.0             #kg/m depth
 psi0 = 0.01448606927    #kg water / kg total
-T0 = 21.0               #C
+T0 = 16.85              #C
 xs = collect(0:50:10000)#m
 hs = @. logslopefunction(xs)#m
 
 psis,Ts,ms,mws = itterate_height(m0,psi0,T0,hs)
-p2 = plot(xs,hs)
-p3 = plot(xs,Ts)
-p1 = percipitationplot(xs,mws)
+mdryairs = @. (1.0 - psis)*ms
+p1 = plot(xs,hs)
+p2 = plot(xs,Ts)
+p3 = percipitationplot(xs,mws)
+p4 = percipitationplot2(xs,mdryairs,mws)
 
 #plot(hs,@. pressure(hs))
--- visualization.jl
+++ visualization.jl
@@ -12,3 +12,20 @@ function percipitationplot(xs,mws)
     push!(pointsy,0.0)
     return plot(pointsx,pointsy, seriestype = :shape,xlabel = "Horizontal Distance (m)",ylabel = "Percipitation (kg/m^2)")
 end
+
+function percipitationplot2(xs,mdryairs,mws)
+    pointsx = [0.0]
+    pointsy = [0.0]
+    totalpercipitation = 0.0
+    for i = 1:1:length(mws)
+        #dist = xs[i+1] - xs[i]
+        totalpercipitation = totalpercipitation + mws[i]
+        push!(pointsx,xs[i])
+        push!(pointsx,xs[i+1])
+        push!(pointsy,totalpercipitation/mdryairs[i])
+        push!(pointsy,totalpercipitation/mdryairs[i])
+    end
+    push!(pointsx,xs[end])
+    push!(pointsy,0.0)
+    return plot(pointsx,pointsy, seriestype = :shape,xlabel = "Horizontal Distance (m)",ylabel = "Accumulated Percipitation (kg/kg)")
+end
